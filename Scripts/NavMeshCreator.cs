6e47a9a99bd89e631e2194e74a7840d9afdec682
csnow <colinmsnow@gmail.com>
2021-04-26 20:14:05 -0400

    Implement tetrahedron with sphere generation

--- Scripts/NavMeshCreator.cs
+++ Scripts/NavMeshCreator.cs
@@ -67,32 +67,46 @@ public class NavMeshCreator : MonoBehaviour{
 
         }
 
-        int possibleTriangles = 0;
-        int clearTriangles = 0;
 
-        // Iterate all possible tetrahedra
-        for (int i=0; i<points.Count;i++){
-            for (int j=0; j<points.Count;j++){
-                for (int k=0; k<points.Count;k++){
-                    for (int l=0; l<points.Count;l++){
+        /* Delauny triangulation
 
-                        possibleTriangles++;
+        Defined as a set of tetrahedra with vertices in S such that the sphere defined by the set of
+        vertices of each tetrahedron does not contain any nodes in S
+
+        Steps:
+
+        1. Create enclosing tetrahedron
+        2. Add point from S
+        3. Iterate through all spheres to check if the sphere contains the point
+            3.1 If the sphere does not, keep the tetrahedron
+            3.2 If it does, delete the tetrahedron and connect all points in the
+                tetrahedron to the current point to create smaller terrahedra.
+        4. Do this for all points in s.
+
+        Runtime:
+
+        Should run in O(n^5/3) for 3d but not sure how they check if points are in stuff etc.
+
+        */
+
+        // double[] point1 = new double[] {0, 0, 0};
+        // double[] point2 = new double[] {1, 1, 1};
+        // double[] point3 = new double[] {2, 5, 7};
+        // double[] point4 = new double[] {3, -4, 6};
+        
+
+        // var solver = new CircumcentreSolver(point1, point2, point3, point4);
+
+        // Debug.Log(solver.Centre[0]);
+        // Debug.Log(solver.Centre[1]);
+        // Debug.Log(solver.Centre[2]);
+        // Debug.Log(solver.Radius);
+
+        var tet = new Tetrahedron(new Vector3(0,0,0), new Vector3(1, 1, 1), new Vector3(5,-3,6), new Vector3(-4, 7, 9));
 
 
-                        if (pathClear(points[i], points[j]) &&
-                            pathClear(points[i], points[k]) &&
-                            pathClear(points[i], points[l]) &&
-                            pathClear(points[j], points[k]) &&
-                            pathClear(points[j], points[l]) &&
-                            pathClear(points[k], points[l]))
-                        {
 
-                            clearTriangles++;
-
-                            
-                            
-
-                        }
+        
 
 
 
@@ -101,14 +115,21 @@ public class NavMeshCreator : MonoBehaviour{
 
 
 
-                    }
-                }
-            }
 
-        }
 
-        Debug.Log(possibleTriangles);
-        Debug.Log(clearTriangles);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // // Iterate through each point and try to connect to each other point
 
@@ -213,5 +234,46 @@ public class NavMeshCreator : MonoBehaviour{
         RaycastHit hit;
         return (Physics.Raycast(point1, point2 - point1, out hit, Vector3.Distance(point1, point2)));
 
+    }
+}
+
+public class Tetrahedron{
+
+    private List<Vector3> vertices = new List<Vector3>();
+    private Vector3 center;
+    private float radius;
+
+    public Tetrahedron(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 vertex4){
+        vertices.Add(vertex1);
+        vertices.Add(vertex2);
+        vertices.Add(vertex3);
+        vertices.Add(vertex4);
+
+
+        var solver = new CircumcentreSolver(vec3todouble(vertex1), vec3todouble(vertex2), vec3todouble(vertex3), vec3todouble(vertex4));
+        center = new Vector3((float) solver.Centre[0], (float) solver.Centre[1], (float) solver.Centre[2]);
+        radius = (float) solver.Radius;
+        Debug.Log(center);
+        Debug.Log(radius);
+    }
+
+    private Vector3 floattovec3(float[] input){
+        return new Vector3 (input[0], input[1], input[2]);
+        
+    }
+
+    private float[] vec3tofloat(Vector3 input){
+        return new float[] {input[0], input[1], input[2]};
+        
+    }
+
+    private Vector3 doubletovec3(double[] input){
+        return new Vector3 ((float) input[0], (float) input[1], (float) input[2]);
+        
+    }
+
+    private double[] vec3todouble(Vector3 input){
+        return new double[] {(double) input[0], (double) input[1], (double) input[2]};
+        
     }
 }
