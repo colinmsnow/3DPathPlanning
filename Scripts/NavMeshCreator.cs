7de2d733ed47d8458b4b1a4543646174d8ca4e0d
csnow <colinmsnow@gmail.com>
2021-04-26 21:37:52 -0400

    Creating tetrahedra but too many of them

--- Scripts/NavMeshCreator.cs
+++ Scripts/NavMeshCreator.cs
@@ -89,6 +89,143 @@ public class NavMeshCreator : MonoBehaviour{
 
         */
 
+
+        Vector3 point1 = new Vector3(0,0,0);
+        Vector3 point2 = new Vector3(1,1,1);
+        Vector3 point3 = new Vector3(1,-3,4);
+        Vector3 point4 = new Vector3(-2,2,1);
+        Vector3 pointnew = new Vector3(-2,3,1);
+        Vector3 pointnew2 = new Vector3(-.319f,1.94f,2.1f);
+
+        List<Vector3> tetpoints = new List<Vector3>();
+        tetpoints.Add(point1);
+        tetpoints.Add(point2);
+        tetpoints.Add(point3);
+        tetpoints.Add(point4);
+        tetpoints.Add(pointnew);
+        tetpoints.Add(pointnew2);
+
+
+        Vector3 point5 = new Vector3(6.25f,-5,-5);
+        Vector3 point6 = new Vector3(5,5,5);
+        Vector3 point7 = new Vector3(-5,5,0);
+        Vector3 point8 = new Vector3(0,-5,5);
+
+        List<Vector3> tetbounding = new List<Vector3>();
+        tetbounding.Add(point5);
+        tetbounding.Add(point6);
+        tetbounding.Add(point7);
+        tetbounding.Add(point8);
+
+
+        // Create initial tetrahedron
+
+
+        var initialtet = new Tetrahedron(point5, point6, point7, point8);
+
+        // initialtet.display();
+
+        List<Tetrahedron> tetrahedra = new List<Tetrahedron>();
+
+        List<Tetrahedron> badTetrahedra = new List<Tetrahedron>();
+
+        tetrahedra.Add(initialtet);
+
+
+        foreach (Vector3 p in tetpoints){
+            // Iterate through points adding one at a time
+
+            Debug.Log("Starting first tetrahedron");
+
+
+            foreach(Tetrahedron tet in tetrahedra){
+                if (tet.contains(p)){
+                    badTetrahedra.Add(tet);
+                    Debug.Log("Found bad tetrahedron");
+                
+                }
+            }
+
+            List<Face> faces = new List<Face>();
+
+            foreach(Tetrahedron tet in badTetrahedra){
+                // Need to add triangle face which is not shared by any other tetrahedron
+
+                var tetfaces = tet.faces();
+
+                foreach(Face face in tetfaces){
+                    if (!faces.Contains (face)){
+                        faces.Add(face);
+                        Debug.Log("Adding a face");
+                    }
+                }
+            }
+
+            foreach(Face face in faces){
+
+                // Create a new tetrahedron using this face and the test point
+                var newtet = new Tetrahedron(p, face.vertices[0], face.vertices[1], face.vertices[2]);
+                tetrahedra.Add(newtet);
+                Debug.Log("Created new tetrahedron");
+            }
+
+            foreach(Tetrahedron tet in badTetrahedra){
+                // Want to remove that tetrahedron and create new ones to fill space
+
+                // Remove
+                tetrahedra.Remove(tet);
+
+            }
+
+
+            Debug.Log("Ending first tetrahedron");
+
+
+        }
+
+
+
+        // Delete tetrahedra which have one of the bounding vertices
+
+        List<Tetrahedron> filteredTetrahedra = new List<Tetrahedron>();
+
+        foreach(Tetrahedron tet in tetrahedra){
+            if (!(tet.hasPoint(point5) || tet.hasPoint(point6) || tet.hasPoint(point7) || tet.hasPoint(point8))){
+                filteredTetrahedra.Add(tet);
+            }
+        }
+
+        foreach(Tetrahedron tet in filteredTetrahedra){
+            tet.display();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // double[] point1 = new double[] {0, 0, 0};
         // double[] point2 = new double[] {1, 1, 1};
         // double[] point3 = new double[] {2, 5, 7};
@@ -102,7 +239,40 @@ public class NavMeshCreator : MonoBehaviour{
         // Debug.Log(solver.Centre[2]);
         // Debug.Log(solver.Radius);
 
-        var tet = new Tetrahedron(new Vector3(0,0,0), new Vector3(1, 1, 1), new Vector3(5,-3,6), new Vector3(-4, 7, 9));
+        // Vector3 point1 = new Vector3(0,0,0);
+        // Vector3 point2 = new Vector3(1,1,1);
+        // Vector3 point3 = new Vector3(5,-3,6);
+        // Vector3 point4 = new Vector3(-4,7,9);
+
+        // List<Vector3> tetpoints = new List<Vector3>();
+        // tetpoints.Add(point1);
+        // tetpoints.Add(point2);
+        // tetpoints.Add(point3);
+        // tetpoints.Add(point4);
+
+
+        foreach (Vector3 a in tetpoints){
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.localScale = new Vector3(.1f, .1f, .1f);
+                cube.transform.position = a;
+                cube.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        foreach (Vector3 a in tetbounding){
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.localScale = new Vector3(.1f, .1f, .1f);
+                cube.transform.position = a;
+                cube.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        // var tet = new Tetrahedron(point1, point2, point3, point4);
+
+
+        // GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //         sphere.transform.localScale = new Vector3(tet.radius*2, tet.radius*2, tet.radius*2);
+        //         sphere.transform.position = tet.center;
+        //         sphere.GetComponent<Renderer>().material.color = Color.blue;
+
 
 
 
@@ -239,9 +409,9 @@ public class NavMeshCreator : MonoBehaviour{
 
 public class Tetrahedron{
 
-    private List<Vector3> vertices = new List<Vector3>();
-    private Vector3 center;
-    private float radius;
+    public List<Vector3> vertices = new List<Vector3>();
+    public Vector3 center;
+    public float radius;
 
     public Tetrahedron(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3, Vector3 vertex4){
         vertices.Add(vertex1);
@@ -255,6 +425,42 @@ public class Tetrahedron{
         radius = (float) solver.Radius;
         Debug.Log(center);
         Debug.Log(radius);
+    }
+
+    public List<Face> faces(){
+        List<Face> facelist = new List<Face>();
+        facelist.Add(new Face(vertices[0], vertices[1], vertices[2]));
+        facelist.Add(new Face(vertices[0], vertices[1], vertices[3]));
+        facelist.Add(new Face(vertices[0], vertices[2], vertices[3]));
+        facelist.Add(new Face(vertices[1], vertices[2], vertices[3]));
+        return facelist;
+    }
+
+    public bool contains(Vector3 point){
+        return (Vector3.Distance(point, center) < radius);
+    }
+
+    public bool hasPoint(Vector3 point){
+        return vertices.Contains(point);
+    }
+
+    public void display(){
+        // Displays a tetrahedron
+
+        foreach (Vector3 vertex in vertices){
+
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.localScale = new Vector3(.2f, .2f, .2f);
+                cube.transform.position = vertex;
+                cube.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+
+        Debug.DrawLine(vertices[0], vertices[1], Color.cyan, 1000, false);
+        Debug.DrawLine(vertices[0], vertices[2], Color.cyan, 1000, false);
+        Debug.DrawLine(vertices[0], vertices[3], Color.cyan, 1000, false);
+        Debug.DrawLine(vertices[1], vertices[2], Color.cyan, 1000, false);
+        Debug.DrawLine(vertices[1], vertices[3], Color.cyan, 1000, false);
+        Debug.DrawLine(vertices[2], vertices[3], Color.cyan, 1000, false);
     }
 
     private Vector3 floattovec3(float[] input){
@@ -275,5 +481,20 @@ public class Tetrahedron{
     private double[] vec3todouble(Vector3 input){
         return new double[] {(double) input[0], (double) input[1], (double) input[2]};
         
+    }
+}
+
+public class Face{
+
+    public List<Vector3> vertices = new List<Vector3>();
+
+    public Face(Vector3 a, Vector3 b, Vector3 c){
+        vertices.Add(a);
+        vertices.Add(b);
+        vertices.Add(c);
+    }
+
+    public bool equals(Vector3 a, Vector3 b, Vector3 c){
+        return (vertices.Contains(a) && vertices.Contains(b) && vertices.Contains(c));
     }
 }
